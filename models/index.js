a205a375f5b9e3d71abd740908c6b7364ea15d31
jeremysr <jeremysr@protonmail.com>
2023-09-16 13:56:41 -0600

    added model assoications to index, but not sure if they are correct yet. will debug later

--- models/index.js
+++ models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey:'id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  onDelete:'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag);
 
 // Tags belongToMany Products (through ProductTag)
+Tag/Product.belongsToMany(Product);
 
 module.exports = {
   Product,

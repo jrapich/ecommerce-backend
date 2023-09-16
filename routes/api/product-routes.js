63b873f5fa49917317ccc6e47fd0d1d9b00d7454
jeremysr <jeremysr@protonmail.com>
2023-09-16 15:30:13 -0600

    added api route to retrieve info for product specified by ID

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,9 +21,21 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id,
+      {
+      include:{
+        all:true
+      }
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
 });
 
 // create new product
